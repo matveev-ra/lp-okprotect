f365443503e7441d50417eacc415650aff986bdb
Roman <ramzez1@gmail.com>
2022-04-05 09:25:37 +0300

    05042022_0925

--- js/custom.js
+++ js/custom.js
@@ -150,6 +150,93 @@ $("form#Forma1").submit(function () {
 });	
 
 
+$(function(){
+
+  $(".WrapTabs .WrapTabs__head .WrapTabs__tab").on("click", function(){
+      var tabs = $(".WrapTabs .WrapTabs__head .WrapTabs__tab")
+          cont = $(".WrapTabs .WrapTabs__body .WrapTabs__cont");
+    // Удаляем классы active
+      tabs.removeClass("active");
+      cont.removeClass("active");
+    // Добавляем классы active
+      $(this).addClass("active");
+      cont.eq($(this).index()).addClass("active");
+      return false;
+  });
+});
+
+
+
+
+
+
 
 });
 //$(document).ready
+
+
+
+// Accordion -1
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+    
+    var dropdownlink = this.el.find('.Acc-Title');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+  };
+  
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the .Acc-Text
+        $next = $this.next();
+    
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.Acc-Text').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  
+  var accordion = new Accordion($('.Accardion'), false);
+})
+
+// Accordion -1
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+    
+    var dropdownlink = this.el.find('.AccExpert__img');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+  };
+  
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the .Acc-Text
+        $next = $this.next();
+    
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.AccExpert__Text').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  
+  var accordion = new Accordion($('.AccExpert'), false);
+})
+
+
+
